f9052424e349fe34d68730bb769faa02168fc159
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-04-16 12:13:30 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,n;
+    printf("enter the no fo rwo");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        for (j=1;j<=n;j++)
+        {
+
+        }
+    }
+}
